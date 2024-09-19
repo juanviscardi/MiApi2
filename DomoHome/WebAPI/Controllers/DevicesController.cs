d98c1c11e6db70639e24a4630dae4fb062350019
Juan Viscardi <juanviscardi@gmail.com>
2024-09-19 14:11:20 -0300

    Algo mas en device

--- DomoHome/WebAPI/Controllers/DevicesController.cs
+++ DomoHome/WebAPI/Controllers/DevicesController.cs
@@ -1,5 +1,6 @@
 ﻿//using BusinessLogic;
 //using Domain;
+using Domain;
 using IBusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 using Models.In;
@@ -72,5 +73,36 @@ namespace WebAPI.Controllers
 
             return Created(string.Empty, response);           
         }
+
+        [HttpDelete("{name}")]
+        public IActionResult DeleteDevice(string name)
+        {
+            List<string> devices = new List<string> { "Camera", "Sensor", "Door", "Window" };
+            var device = devices.FirstOrDefault(d => d.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (device == null)
+            {
+                return NotFound(new { message = "Device not found" });
+            }
+
+            devices.Remove(device);
+            return NoContent();  // 204 No Content
+        }
+
+       /* [HttpPut("{name}")]
+        public IActionResult UpdateDevice(string name, [FromBody] UpdateDeviceRequest device)
+        {
+            List<string> devices = new List<string> { "Camera", "Sensor", "Door", "Window" };
+            var deviceToUpdate = devices.FirstOrDefault(d => d.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (deviceToUpdate == null)
+            {
+                return NotFound(new { message = "Device not found" });
+            }
+
+            devices.Remove(deviceToUpdate);
+            devices.Add(device.Name);
+            return NoContent();  // 204 No Content
+        }*/
+
+
     }
 }
